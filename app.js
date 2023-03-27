95c12c05bd661b436ccf31d6c850e2e36d3e17b5
lauren-gessel <gessel.14@buckeyemail.osu.edu>
2023-03-27 09:47:26 -0400

    mugdrawing

--- app.js
+++ app.js
@@ -1,37 +1,65 @@
 function setup(){
     createCanvas(720, 400);
-}
-function draw(){
-    background(150);
-
-
-
+    background(100,55,40);
+    fill(100,70,50);
     noStroke();
-    fill(50,100,50);
-    rect(0, 300, 750, 100);
+    rect(20, 20, 680, 100);
+    rect(20, 140, 680, 100);
+    rect(20, 260, 680, 120);
+   // drawFace(100, 200, 30);  // left face with bigger eyes 
+   // drawFace(300, 200, 10);  // right face
+}
+//function drawFace(x, y, eyeSize) {
+ //   push();
+ //   fill(110, 180, 100);
+ //   translate(x,y);
+ //   ellipse(0, 10, 100, 100); //head
+ //   drawEye(-15, -10, eyeSize);
+ //   drawEye(15, -10, eyeSize);
+ //   fill(37,120,97);
+ //   rect(-30, 30, 60,5); //mouth
+ //   pop();
+  //}
+  
+ // function drawEye(x,y, eyeSize) {
+ //   push();
+ //   translate(x,y);
+ //   fill(255); //white
+ //   ellipse(0, 6, eyeSize+10, eyeSize+10);
+ //   fill(10,100,10); //black
+ //   ellipse(-3, 0, eyeSize/4.0, eyeSize/4.0);
+    
+  //  pop();
+  //}
+  function drawBody (c) {
+    fill(c);
+    noStroke();
+    strokeWeight(1);    
+    arc(50, 50, 80, 100, 0, PI);
+  }
+  function drawMug(c, drawHandle) {
+    push();
+    noFill();
+    strokeWeight(10);
+    stroke(100);
+    curve(1, 60, 90, 55, 83, 84, 1, 100);
+    pop();
+  }
+function draw(){
+  //background(200);
+    drawMug();
+    drawBody(100);
+ strokeWeight(1);
+//fill(100,10,40);
+//strokeWeight(1);    
+//arc(50, 50, 80, 100, 0, PI);
 
-    fill(20,200,20)
-    ellipse(106, 146, 80, 55);
+//noFill();
+//strokeWeight(10);
+//stroke(100,10,40);
+//curve(1, 60, 90, 55, 83, 84, 1, 100);
 
-    quad(120, 170, 136, 136, 300, 190, 270, 235);
-   
-    ellipse(350,220, 250, 120) 
-
-    quad(235, 240, 275, 265, 255, 345, 230, 345);
-
-    quad(425, 200, 470, 230, 455, 345, 430, 345);
-
-    triangle(470, 230, 520, 200, 465, 200);
-
-    fill(200,100, 50);
-    triangle(593, 85, 645, 62, 612, 106);
-
-    fill(100, 0, 100);
-    circle(600, 100, 30, 30);
-// animate above two, to grow and or move with time
-    fill(0);
-    circle(100, 150, 10, 10);
-    fill(100);
+//strokeWeight(.5);
     helperCoordinates();
 }
 
